f85c4737c46ae49d871a65fb2dd9473a055f80c1
lzxzz <1042183935@qq.com>
2022-10-23 16:33:51 +0800

    deploy

---
+++ precache-manifest.a11c65d62b5e9701e59a38c52c9f62bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a7d0ac5a7195179a14bb76f159c46b8",
+    "url": "/react-bookkeeping-website/index.html"
+  },
+  {
+    "revision": "c6aafeddd7d0d2e9919c",
+    "url": "/react-bookkeeping-website/static/css/main.07be7270.chunk.css"
+  },
+  {
+    "revision": "53204d65b0ea47da6755",
+    "url": "/react-bookkeeping-website/static/js/2.11665e6b.chunk.js"
+  },
+  {
+    "revision": "532dd96e6c8a5e956f36ad4e9ee5e70f",
+    "url": "/react-bookkeeping-website/static/js/2.11665e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6aafeddd7d0d2e9919c",
+    "url": "/react-bookkeeping-website/static/js/main.d25caa43.chunk.js"
+  },
+  {
+    "revision": "f21f91c3e627c405e0e9",
+    "url": "/react-bookkeeping-website/static/js/runtime-main.36e86849.js"
+  },
+  {
+    "revision": "095613483d5b8e4639c1eaa249847b07",
+    "url": "/react-bookkeeping-website/static/media/bookkeeping.09561348.jpg"
+  }
+]);
